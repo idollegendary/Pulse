7abdb2140c2f2ddb217a1844b5776106a8794ac0
kop1s <idollegendary@gmail.com>
2021-12-29 14:49:21 +0200

    lesson52

---
+++ src/js/script.js
@@ -0,0 +1,72 @@
+var name = "Ivan";
+
+let namber = 7;
+const pi = 3.14;
+
+number = 4;
+
+let leftBorderWidth;
+
+// number
+// string - "", '', ``
+// true/false
+// null
+// undefined
+// let obj = {
+//     name: 'apple',
+//     color: 'green',
+//     weight: 200
+// }
+// Symbol
+
+// alert(1234)
+// console.log(number)
+// let answer = confirm("вам есть 18?")
+// console.log(answer)
+
+// let answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+// console.log(4 + 'as');
+
+// let isChecked = false,
+//     isClose = false;
+
+// console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
+
+// if (2*6 == 8*1) {
+//     console.log('верно')
+// } else {
+//     console.log('ошибка')
+// }
+
+// let answer = confirm("вам есть 18?")
+// if (answer) {
+//     console.log('проходите')
+// } else {
+//     console.log('уходи')
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('неправильно')
+// } else if (num > 100) {
+//     console.log('много')
+// } else {
+//     console.log('верно')
+// }
+
+// for(let i = 1; i < 8; i++) {
+//     console.log(i)
+// }
+
+function logging(a, b) {
+    console.log( a + b)
+}
+
+logging(3, 5);
+
+logging(6, 8);
