b1df8922b34c16dba447b702f93afed8a472f574
kop1s <idollegendary@gmail.com>
2021-12-31 00:21:08 +0200

    lesson 54

--- src/js/script.js
+++ src/js/script.js
@@ -1,72 +1,18 @@
-var name = "Ivan";
-
-let namber = 7;
-const pi = 3.14;
-
-number = 4;
-
-let leftBorderWidth;
-
-// number
-// string - "", '', ``
-// true/false
-// null
-// undefined
-// let obj = {
-//     name: 'apple',
-//     color: 'green',
-//     weight: 200
-// }
-// Symbol
-
-// alert(1234)
-// console.log(number)
-// let answer = confirm("вам есть 18?")
-// console.log(answer)
-
-// let answer = prompt("Вам есть 18?", "");
-// console.log(answer);
-
-// console.log(4 + 'as');
-
-// let isChecked = false,
-//     isClose = false;
-
-// console.log(isChecked && isClose);
-
-// console.log(isChecked || isClose);
-
-// if (2*6 == 8*1) {
-//     console.log('верно')
-// } else {
-//     console.log('ошибка')
-// }
-
-// let answer = confirm("вам есть 18?")
-// if (answer) {
-//     console.log('проходите')
-// } else {
-//     console.log('уходи')
-// }
-
-// const num = 50;
-
-// if (num < 49) {
-//     console.log('неправильно')
-// } else if (num > 100) {
-//     console.log('много')
-// } else {
-//     console.log('верно')
-// }
-
-// for(let i = 1; i < 8; i++) {
-//     console.log(i)
-// }
-
-function logging(a, b) {
-    console.log( a + b)
-}
-
-logging(3, 5);
-
-logging(6, 8);
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+      });
+  });
+      
